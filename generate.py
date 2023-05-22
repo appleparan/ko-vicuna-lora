4295028d1518024ca1c8144aacbcbf4e118603b8
Jongsu "Liam" Kim <jongsukim8@gmail.com>
2023-05-23 02:20:20 +0900

    Update generate.py to Redpajama model

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ import gradio as gr
 import torch
 import transformers
 from peft import PeftModel
-from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+from transformers import GenerationConfig, AutoTokenizer, AutoModelForCausalLM
 
 from utils.callbacks import Iteratorize, Stream
 from utils.prompter import Prompter
@@ -25,9 +25,9 @@ except:  # noqa: E722
 
 def main(
     load_8bit: bool = False,
-    base_model: str = "",
+    base_model: str = "togethercomputer/RedPajama-INCITE-Chat-7B-v0.1",
     lora_weights: str = "tloen/alpaca-lora-7b",
-    prompt_template: str = "",  # The prompt template to use, will default to alpaca.
+    prompt_template: str = "redpajama",  # The prompt template to use, will default to alpaca.
     server_name: str = "0.0.0.0",  # Allows to listen on all interfaces by providing '0.
     share_gradio: bool = False,
 ):
@@ -37,9 +37,11 @@ def main(
     ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
 
     prompter = Prompter(prompt_template)
-    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+
     if device == "cuda":
-        model = LlamaForCausalLM.from_pretrained(
+        model = AutoModelForCausalLM.from_pretrained(
             base_model,
             load_in_8bit=load_8bit,
             torch_dtype=torch.float16,
@@ -51,10 +53,8 @@ def main(
             torch_dtype=torch.float16,
         )
     elif device == "mps":
-        model = LlamaForCausalLM.from_pretrained(
-            base_model,
-            device_map={"": device},
-            torch_dtype=torch.float16,
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model, torch_dtype=torch.float16, device_map={"": device}
         )
         model = PeftModel.from_pretrained(
             model,
@@ -63,8 +63,8 @@ def main(
             torch_dtype=torch.float16,
         )
     else:
-        model = LlamaForCausalLM.from_pretrained(
-            base_model, device_map={"": device}, low_cpu_mem_usage=True
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model, low_cpu_mem_usage=True, device_map={"": device}
         )
         model = PeftModel.from_pretrained(
             model,
@@ -87,76 +87,43 @@ def main(
     def evaluate(
         instruction,
         input=None,
-        temperature=0.1,
+        temperature=0.7,
         top_p=0.75,
-        top_k=40,
-        num_beams=4,
+        top_k=5,
         max_new_tokens=128,
+        do_sample=True,
         stream_output=False,
         **kwargs,
     ):
         prompt = prompter.generate_prompt(instruction, input)
-        inputs = tokenizer(prompt, return_tensors="pt")
-        input_ids = inputs["input_ids"].to(device)
+        inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+        input_length = inputs.input_ids.shape[1]
         generation_config = GenerationConfig(
             temperature=temperature,
             top_p=top_p,
             top_k=top_k,
-            num_beams=num_beams,
+            do_sample=do_sample,
+            max_new_tokens=max_new_tokens,
             **kwargs,
         )
 
         generate_params = {
-            "input_ids": input_ids,
             "generation_config": generation_config,
             "return_dict_in_generate": True,
             "output_scores": True,
-            "max_new_tokens": max_new_tokens,
         }
-
-        if stream_output:
-            # Stream the reply 1 token at a time.
-            # This is based on the trick of using 'stopping_criteria' to create an iterator,
-            # from https://github.com/oobabooga/text-generation-webui/blob/ad37f396fc8bcbab90e11ecf17c56c97bfbd4a9c/modules/text_generation.py#L216-L243.
-
-            def generate_with_callback(callback=None, **kwargs):
-                kwargs.setdefault(
-                    "stopping_criteria", transformers.StoppingCriteriaList()
-                )
-                kwargs["stopping_criteria"].append(
-                    Stream(callback_func=callback)
-                )
-                with torch.no_grad():
-                    model.generate(**kwargs)
-
-            def generate_with_streaming(**kwargs):
-                return Iteratorize(
-                    generate_with_callback, kwargs, callback=None
-                )
-
-            with generate_with_streaming(**generate_params) as generator:
-                for output in generator:
-                    # new_tokens = len(output) - len(input_ids[0])
-                    decoded_output = tokenizer.decode(output)
-
-                    if output[-1] in [tokenizer.eos_token_id]:
-                        break
-
-                    yield prompter.get_response(decoded_output)
-            return  # early return for stream_output
 
         # Without streaming
         with torch.no_grad():
             generation_output = model.generate(
-                input_ids=input_ids,
+                **inputs,
                 generation_config=generation_config,
                 return_dict_in_generate=True,
-                output_scores=True,
                 max_new_tokens=max_new_tokens,
             )
-        s = generation_output.sequences[0]
-        output = tokenizer.decode(s)
-        yield prompter.get_response(output)
+        s = generation_output.sequences[0, input_length:]
+        output_str = tokenizer.decode(s)
+        yield prompter.get_response(output_str)
 
     gr.Interface(
         fn=evaluate,
